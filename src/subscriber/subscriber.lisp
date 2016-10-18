f3e1ac78a6b29d5c6861d36a30d2fc767b73a8af
yorodm <yoandy.rmartinez@gmail.com>
2016-10-18 12:10:56 -0400

    Better this way

--- src/subscriber/subscriber.lisp
+++ src/subscriber/subscriber.lisp
@@ -15,68 +15,53 @@
     :documentation "Called when the observable finishes processing values"))
   (:documentation "A subscriber"))
 
-(defmacro defsubscriber (name &rest body))
-
 (defclass safe-subscriber (subscriber) ;; this is internal
   ((inner :initarg :inner :accesor inner)
    (state :initarg :state :accespr state :initform :live))
-  (:documentation "A class to wrap user created subscriber"))
+  (:documentation "A class to wrap user created subscribers"))
 
-(defun parse-body (body)
-  "The old parse-body, it makes sure that body has the form is suitable for
-defining a new function using flet"
-  ())
-
-(defun parse-body (body &key documentation whole)
-  "Parses BODY into (values remaining-forms declarations doc-string).
-Documentation strings are recognized only if DOCUMENTATION is true.
-Syntax errors in body are signalled and WHOLE is used in the signal
-arguments when given."
-  (let ((doc nil)
-        (decls nil)
-        (current nil))
-    (tagbody
-     :declarations
-       (setf current (car body))
-       (when (and documentation (stringp current) (cdr body))
-         (if doc
-             (error "Too many documentation strings in ~S." (or whole body))
-             (setf doc (pop body)))
-         (go :declarations))
-       (when (and (listp current) (eql (first current) 'declare))
-         (push (pop body) decls)
-         (go :declarations)))
-    (values body (nreverse decls) doc)))
-
-(defmacro make-subscriber &key on-next on-completed on-error
-          "Creates a new subscriber given at least one of it functions. The parameters are:
-ON-NEXT: A function that takes two parameters. The SUBSCRIBER and the item generated
-by the OBSERVABLE.
-ON-ERROR: A function that takes two parameters, the SUBSCRIBER and a function receiving
-the condition as a parameter.
-ON-COMPLETED: A function that takes a SUBSCRIBER as a parameter.
-This function is the only safe way to create a subscriber so do not call make-instance."
-
-          (safe-subscriber (make-instance 'subscriber :on-next on-next
-                                          :on-completed on-completed
-                                          :on-error on-error)))
 
 (defun safe-subscriber (inner)
   ;; this is basically a state machine
   "Wraps SUB into a safe subscriber. The safe subscriber guarantees that
 SUB behaves acordingly to observable semantics"
   (flet ((on-next (sub it)
-           (case ()
-               ))
-         (on-error (cnd) (values))
-         (on-complete (sub)))
+           (case (state sub)
+             (:live (subscriber-next sub it))
+             (otherwise (values))))
+         (on-error (cnd)
+           (case (state sub)
+             (:live (setf (state sub) :error) (subscriber-error sub cnd))
+             (otherwise (values))))
+         (on-complete (sub)
+           (case (state sub)
+             (:live (setf (state sub) :completed))
+             (otherwise (values)))))
     (make-instance 'safe-subscriber
                    :inner inner
                    :on-next #'on-next
                    :on-error #'on-error
                    :on-complete #'on-complete)))
 
-;; Need generic functions to cover the case of safe subscriber
+;; public interface
+
+(defmacro make-subscriber &key on-next on-completed on-error
+          "Creates a new subscriber given at least one of it functions. The parameters are:
+ON-NEXT: Notifies the SUBSCRIBER of new events in the OBSERVABLE. Takes the item as parameter.
+ON-ERROR: Notifies the SUBSCRIBER of errors in the OBSERVABLE. Takes the error as parameter.
+ON-COMPLETED: Notifies the SUBSCRIBER that the OBSERVABLE will no longer send events it's way.
+
+This function is the only safe way to create a subscriber so do not call make-instance."
+
+          (safe-subscriber (make-instance 'subscriber :on-next on-next
+                                          :on-completed on-completed
+                                          :on-error on-error)))
+
+(defgeneric subscriber-next (sub item))
+
+(defgeneric subscriber-completed (sub))
+
+(defgeneric subscriber-error (sub cond))
 
 (defmethod subscriber-next ((sub subscriber) item)
   (funcall (on-next sub) item))
