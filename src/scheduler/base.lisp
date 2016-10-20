d4ec78205a1b31280881a422b6a1e68c87da8a57
yorodm <yoandy.rmartinez@gmail.com>
2016-10-20 12:46:01 -0400

    Adding some generics

--- src/scheduler/base.lisp
+++ src/scheduler/base.lisp
@@ -9,9 +9,10 @@
 (defgeneric schedule (scheduler fn)
   (:documentation "Schedules a new FN to be executed by the SCHEDULER"))
 
-(defgeneric schedule-recursive(scheduler fn)
- (:documentation "Schedules FN to be executed recursively, FN recieve a single
+(defgeneric schedule-recursive(scheduler action)
+ (:documentation "Schedules ACTION to be executed recursively, FN recieve a single
  parameter. That parameter will be a function used to trigger the recursion "))
 
-(defun invoke-action (fn)
-  (fn *current-scheduler*))
+(defgeneric invoke-action (scheduler action)
+  (:documentation "Executes the action in the current scheduler,
+ACTION must be a function that takes no mandatory parameters"))
