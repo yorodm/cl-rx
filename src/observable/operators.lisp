84493413fa1f8a895a0a9a8587f4c53edebc9b68
yorodm <yoandy.rmartinez@gmail.com>
2016-10-22 18:53:29 -0400

    Adding some comments

--- src/observable/operators.lisp
+++ src/observable/operators.lisp
@@ -6,11 +6,16 @@
 
 
 ;; There are two ways we can go here:
-;; 1) The RxJava way when we set producers and
-;; 2) The RxPython
+;; We can either loop over the whole thing calling SUBSCRIBER-NEXT or
+;; we can provide a safer way by providing a callback to "recursively"
+;; generate events. LOOP seems like a poor choice given that I don't
+;; know if it will be thread safe. Maybe I can set a lot of locks but
+;; that might have an impact in performance.
 (defmethod observable-from ((source list))
   (labels ((from-list (subs) ;; this is the subscribe function
              (labels ((recursive (callback state)
+                        ;; is pop safe for threading?
+                        ;; I should lock on this
                         (let ((item (pop source)))
                           (cond ((null item) (suscriber-complete subs)
                                  (t (progn
