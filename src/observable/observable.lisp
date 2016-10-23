2a47417fd059706789aa7aa20758f88980613b52
yorodm <yoandy.rmartinez@gmail.com>
2016-10-23 14:14:10 -0400

    The debate about whether or not we should make subscribe a multimethod is over now. Bottomline: If you have CLOS there's no point in abusing closures.

--- src/observable/observable.lisp
+++ src/observable/observable.lisp
@@ -4,13 +4,10 @@
   ((subscribe-fn :initarg :subscribe :initform nil))
   (:documentation "The base class for all observables"))
 
-(defgeneric observable-from (source)
-  (:documentation "Creates a new observable from the given source"))
-
-(defgeneric observable-subscribe (subscriber)
+(defgeneric observable-subscribe (observable subscriber)
   (:documentation "Subscribes a new subscriber to the observable"))
 
 (defun make-observable (fn)
   "Creates an observable that will execute FN when a SUBSCRIBER
- subscribes to it. FN must comply with the observable contract."
+ subscribes to it."
   (make-instance 'observable :subscribe fn))
