d670894f031e6d4cbdc10289de819db2a24c0344
yorodm <yoandy.rmartinez@gmail.com>
2016-10-24 22:40:46 -0400

    There's no need for INVOKE-ACTION

--- src/scheduler/base.lisp
+++ src/scheduler/base.lisp
@@ -12,10 +12,6 @@
 (defgeneric schedule-relative (scheduler fn time)
   (:documentation "Schedules a new FN to be executed after TIME seconds"))
 
-(defgeneric invoke-action (scheduler action)
-  (:documentation "Executes the action in the current scheduler,
-ACTION must be a function that takes no mandatory parameters"))
-
 (defmacro with-current-scheduler (scheduler &body body)
   `(let ((,scheduler *current-scheduler*))
      ,@body))
