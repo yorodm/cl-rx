dd59c196e89c0a2c95b392ef534beaf15212437a
yorodm <yoandy.rmartinez@gmail.com>
2016-10-24 22:43:34 -0400

    Like I said, no INVOKE-ACTION

--- src/scheduler/immediate.lisp
+++ src/scheduler/immediate.lisp
@@ -4,12 +4,8 @@
   ()
   (:documentation "Executes the work immediately in the current thread"))
 
-;; Method or function? that is the question
-(defmethod invoke-action ((scheduler immediate-scheduler) action)
-  (funcall action))
-
 (defmethod schedule ((scheduler immediate-scheduler) fn)
-  (invoke-action scheduler fn))
+  (funcall fn))
 
 (defmethod schedule-relative ((scheduler immediate-scheduler) fn time)
   (warn "This operation will block the current thread")
