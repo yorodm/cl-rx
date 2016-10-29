e4233aa7a4ca2570fe1672d03e28ae7b6550134b
yorodm <yoandy.rmartinez@gmail.com>
2016-10-29 14:28:43 -0400

    Minor housekeeping

--- src/subscriber/subscriber.lisp
+++ src/subscriber/subscriber.lisp
@@ -32,7 +32,13 @@
   (setf (slot-value instance 'on-error) on-error)
   (setf (slot-value instance 'on-completed) on-completed))
 
+(defmethod make-load-form ((self subscriber) &optional env)
+  (make-load-form-saving-slots self
+                               :slot-names '(on-next on-error on-completed)))
 
+(defmethod make-load-form ((self safe-subscriber) &optional env)
+  (make-load-form-saving-slots self
+                               :slot-names '(inner on-next on-error on-completed)))
 
 (defun safe-subscriber (inner)
   ;; this is basically a state machine
