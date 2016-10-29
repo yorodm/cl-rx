42007fae5dd898de39a83ff6e527bac9baacbeac
yorodm <yoandy.rmartinez@gmail.com>
2016-10-29 14:32:48 -0400

    observable-subscribe as a multi-method

--- src/observable/observable.lisp
+++ src/observable/observable.lisp
@@ -7,6 +7,15 @@
 (defgeneric observable-subscribe (observable subscriber)
   (:documentation "Subscribes a new subscriber to the observable"))
 
+;; Go multi or go home
+(defmethod observable-subscribe ((observable observable)
+                                 (subscriber safe-subscriber))
+  (funcall (subscribe-fn observable)  subscriber))
+
+;; So we can call this using only the on-next function
+(defmethod observable-subscribe ((observable observable)
+                                 (subscriber function))
+  (funcall (subscribe-fn observable) (make-subscriber :on-next subscriber)))
 (defun make-observable (fn)
   "Creates an observable that will execute FN when a SUBSCRIBER
  subscribes to it."
