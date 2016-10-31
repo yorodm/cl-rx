677976b237e7b9108a58eedd224ad76ef6ddce63
yorodm <yoandy.rmartinez@gmail.com>
2016-10-31 04:32:56 -0400

    Making comments pretty since....

--- src/observable/from.lisp
+++ src/observable/from.lisp
@@ -1,11 +1,10 @@
 (in-package :cl-rx.observable)
 
-;; There are two ways we can go here:
-;; We can either loop over the whole thing calling SUBSCRIBER-NEXT or
-;; we can provide a safer way by providing a callback to "recursively"
-;; generate events. LOOP seems like a poor choice given that I don't
-;; know if it will be thread safe. Maybe I can set a lot of locks but
-;; that might have an impact in performance.
+;; There are two ways we can go here: We can either loop over the whole thing
+;; calling SUBSCRIBER-NEXT or we can provide a safer way by providing a callback
+;; to "recursively" generate events. LOOP seems like a poor choice given that I
+;; don't know if it will be thread safe. Maybe I can set a lot of locks but that
+;; might have an impact in performance.
 (defmethod observable-from ((source list))
   (labels ((from-list (subs) ;; this is the subscribe function
              (flet ((producer() ;; this will be called by the scheduler
