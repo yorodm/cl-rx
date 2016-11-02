36855a71ebdecd75b82aabd7cefd664019b83bdb
yorodm <yoandy.rmartinez@gmail.com>
2016-11-02 15:55:40 -0400

    Fixing relative scheduling

--- src/scheduler/trampoline.lisp
+++ src/scheduler/trampoline.lisp
@@ -14,7 +14,7 @@
   (get-internal-real-time))
 
 (defmethod schedule ((scheduler trampoline-scheduler) fn)
-  (schedule-relative scheduler fn (time-delta scheduler 0)))
+  (schedule-relative scheduler fn  0))
 
 (defmethod schedule-relative ((scheduler trampoline-scheduler) fn delay)
   (task-push scheduler fn delay)
@@ -23,14 +23,16 @@
 (defun task-push (scheduler fn delay)
   "Adds a task to the scheduler"
   ;; We need to lock in case we're running inside some other scheduler
+  (let ((delta (time-delta scheduler delay)))
   (bt:with-lock-held ((lock scheduler))
-    (enqueue (queue scheduler) (time-delta scheduler delay) fn)))
+    (enqueue (queue scheduler) delta fn))))
 
 ;; do not, I repeat, do not lock on this method.
 (defun task-run (scheduler)
   (loop while (> (size (queue scheduler)) 0)
      do (let* ((item (dequeue (queue scheduler)))
-              (diff (- (time-now scheduler) (item-priority item))))
+               (now (time-now scheduler))
+               (diff (- (item-priority item) now)))
           (when (> diff 0)
             (sleep (floor diff 1000))) ;; sleep it off
             (funcall (item-data item)))))
