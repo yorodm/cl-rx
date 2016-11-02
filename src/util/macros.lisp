230a2322cba9308748651d26f29916611a37a6ac
yorodm <yoandy.rmartinez@gmail.com>
2016-11-02 01:44:17 -0400

    Threading macro (we'll be needing this)

--- src/util/macros.lisp
+++ src/util/macros.lisp
@@ -1,1 +1,14 @@
 (in-package :cl-rx.util)
+
+(defmacro -> (what &body body)
+  "Clojure style threading macro"
+  (loop
+     with x = what and form = (pop body)
+     while (not (null form))
+       when (listp form)
+     do (setf x `(,(first form) ,x ,@(rest form))
+              form (pop body))
+       else
+         do (setf x `(form x) form (pop body))
+       end
+     finally (return x)))
