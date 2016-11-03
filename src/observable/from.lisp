47d30a0683d1b55d28adee611ae91c8f7eafbe4e
yorodm <yoandy.rmartinez@gmail.com>
2016-11-03 00:38:01 -0400

    observable-from arrays and streams

--- src/observable/from.lisp
+++ src/observable/from.lisp
@@ -16,3 +16,38 @@
                (with-current-scheduler (scheduler)
                  (schedule scheduler #'producer)))))
     (make-observable #'from-list)))
+
+;; Yeah, different words in loop so...
+(defmethod observable-from ((source array))
+  (labels ((from-list (subs) ;; this is the subscribe function
+             (flet ((producer() ;; this will be called by the scheduler
+                      (handler-case (progn
+                                        (loop for item across source
+                                           do (subscriber-next subs item))
+                                        (subscriber-completed subs))
+                        (error (cnd) (subscriber-error subs cnd)))))
+               (with-current-scheduler (scheduler)
+                 (schedule scheduler #'producer)))))
+    (make-observable #'from-list)))
+
+;; This would be a lot easier if the whole gray streams thingy had made it to
+;; the standard. I'm not going to use trivial-gray-streams just for this.
+;;
+(defmacro read-stream (source eof-error-p &optional eof-value)
+  "Read from a stream"
+  `(if (subtypep 'character (stream-element-type ,source))
+      (read-char ,source ,eof-error-p ,eof-value)
+      (read-byte ,source ,eof-error-p ,eof-value)))
+
+(defmethod observable-from ((source stream))
+  (labels ((from-stream (subs) ;; this is the subscribe function
+             (flet ((producer() ;; this will be called by the scheduler
+                      (handler-case (progn
+                                        (loop for byte = (read-stream source nil)
+                                           while byte
+                                           do (subscriber-next subs byte))
+                                        (subscriber-completed subs))
+                        (error (cnd) (subscriber-error subs cnd)))))
+               (with-current-scheduler (scheduler)
+                 (schedule scheduler #'producer)))))
+    (make-observable #'from-stream)))
