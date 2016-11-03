8ed4e3f0496d231dfea9f36de15b80cadd93be29
yorodm <yoandy.rmartinez@gmail.com>
2016-11-03 00:38:42 -0400

    Tests for all the operators so far

--- t/operators-from.lisp
+++ t/operators-from.lisp
@@ -5,5 +5,29 @@
 
 
 ;; blah blah blah.
-(plan nil)
+(plan 3)
+(setf *current-scheduler* (create-scheduler 'immediate-scheduler))
+(defparameter *source-list* (list 1 2 3 4 5 6 7 8 9))
+(defparameter *source-stream* (make-string-input-stream "This is the data"))
+(defparameter *source-array* (make-array 5 :initial-contents '(1 2 3 4 5)))
+
+(subtest "Testing observable-from with a list"
+  (let ((observ (observable-from *source-list*))
+        (*special-var* nil))
+    (observable-subscribe observ (lambda (x) (push x *special-var*)))
+    (is *special-var* (reverse (list 1 2 3 4 5 6 7 8 9)))))
+
+(subtest "Testing observable-from with an array"
+  (let ((observ (observable-from *source-array*))
+        (*special-var* nil))
+    (observable-subscribe observ (lambda (x) (push x *special-var*)))
+    (is *special-var* (reverse (list 1 2 3 4 5)))))
+
+(subtest "Testing observable-from with a stream"
+  (let ((observ (observable-from *source-stream*))
+        (count-var 0))
+    (observable-subscribe observ (lambda (x)
+                                   (declare (ignore x))
+                                   (incf count-var)))
+    (is count-var (length "This is the data"))))
 (finalize)
