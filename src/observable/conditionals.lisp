5bc0800d7a8d0bfdf02f083d2fba62c83a1aac61
yorodm <yoandy.rmartinez@gmail.com>
2016-11-04 19:09:41 -0400

    Lotta stuff

--- src/observable/conditionals.lisp
+++ src/observable/conditionals.lisp
@@ -1,30 +1,54 @@
 (in-package :cl-rx.observable)
 
 (defmethod observable-find ((observ observable) (predicate function))
-  (flet ((subscribe-fn (subs)
-             (flet ((on-next (item)
-                      (when (funcall predicate item)
-                        (subscriber-next subs item)))
-                    (on-completed ()
-                      (subscriber-completed subs))
-                    (on-error (cnd)
-                      (subscriber-error subs cnd)))
-             (observable-subscribe observ
-                                   (make-subscriber :on-next #'on-next
-                                                    :on-error #'on-error
-                                                    :on-completed #'on-completed)))))
+  (flet
+      ((subscribe-fn (subs)
+         (flet
+             ((on-next (item)
+                (when (funcall predicate item)
+                  (subscriber-next subs item)))
+              (on-completed ()
+                (subscriber-completed subs))
+              (on-error (cnd)
+                (subscriber-error subs cnd)))
+           (observable-subscribe observ
+                                 (make-subscriber :on-next #'on-next
+                                                  :on-error #'on-error
+                                                  :on-completed #'on-completed)))))
     (make-observable #'subscribe-fn)))
 
 (defmethod observable-take ((observ observable) (n integer))
-  (flet ((subscribe-fn (subs)
-           (let ((counter 0))
+  (flet
+      ((subscribe-fn (subs)
+         (let ((counter 0))
+           (flet
+               ((on-next (item)
+                  (if (< counter n)
+                      (progn
+                        (subscriber-next subs item)
+                        (incf counter))
+                      (subscriber-completed subs)))
+                (on-completed ()
+                  (subscriber-completed subs))
+                (on-error (cnd)
+                  (subscriber-error subs cnd)))
+             (observable-subscribe observ
+                                   (make-subscriber :on-next #'on-next
+                                                    :on-error #'on-error
+                                                    :on-completed #'on-completed))))))
+    (make-observable #'subscribe-fn)))
+
+(defmethod observable-first (observ observable)
+  (observable-take observ 1))
+
+(defmethod observable-all ((observ observable) (predicate function))
+  (flet
+      ((subscribe-fn (subs)
+         (let ((result t))
            (flet ((on-next (item)
-                    (if (< counter n)
-                        (progn
-                          (subscriber-next subs item)
-                          (incf counter))
-                       (subscriber-completed subs)))
+                    (setf result (and (funcall predicate item) result)))
                   (on-completed ()
+                    (subscriber-next subs result)
                     (subscriber-completed subs))
                   (on-error (cnd)
                     (subscriber-error subs cnd)))
@@ -32,4 +56,4 @@
                                    (make-subscriber :on-next #'on-next
                                                     :on-error #'on-error
                                                     :on-completed #'on-completed))))))
-          (make-observable #'subscribe-fn)))
+        (make-observable #'subscribe-fn)))
