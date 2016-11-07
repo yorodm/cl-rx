69ca5c8c81c9b27d2a4390381ff8d7f03e5015e7
yorodm <yoandy.rmartinez@gmail.com>
2016-11-07 16:59:51 -0500

    Adding more tests

--- t/operators-filter.lisp
+++ t/operators-filter.lisp
@@ -10,10 +10,18 @@
         (*special-var* nil))
     (observable-subscribe observ (lambda (x) (push x *special-var*)))
     (is *special-var* '(9 7 5 3 1))))
-(subtest "Testing the take operator"
+(subtest "Testing the TAKE operator"
   (let ((observ (-> (observable-from *source-list*)
                   (observable-take 3)))
         (*special-var* nil))
     (observable-subscribe observ (lambda (x) (push x *special-var*)))
     (is *special-var* '(3 2 1))))
+(subtest "Testing the ALL operator"
+  (let ((observ (-> (observable-from *source-list2*)
+                  (observable-all #'evenp)))
+        (*special-var* nil))
+    (observable-subscribe observ (lambda (x)
+                                   ;; So we make sure only one value is generated
+                                   (push x *special-var*)))
+    (is *special-var* '(t))))
 (finalize)
